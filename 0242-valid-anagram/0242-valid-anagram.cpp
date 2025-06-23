2cc7b41975b2050549fd841d471a759613fbea2c
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2025-06-23 22:16:50 +0530

    Time: 0 ms (100%), Space: 9.6 MB (74.22%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(t.length()!=s.length()) return false;
+       int counts[26] = {0};
+        for (int i = 0; i < s.length(); i++) { 
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;
+        return true;
+    }
+};
