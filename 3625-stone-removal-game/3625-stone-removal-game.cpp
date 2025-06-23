2b188299208b77780c83312daaa818540a7657b1
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2025-06-23 22:25:41 +0530

    Time: 0 ms (100%), Space: 7.9 MB (15.71%) - LeetHub

---
+++ 3625-stone-removal-game/3625-stone-removal-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canAliceWin(int n) {
+        bool win=0;
+
+      for(int i=10;i>0;i--){
+           if (n - i < 0){
+              return win;
+           }
+        n -= i;
+          win=!win;
+      }
+        return win;
+    }
+};
