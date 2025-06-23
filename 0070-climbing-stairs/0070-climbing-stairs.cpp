bc473bfb888052f69a6ac049c9cf34285b8bb702
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2025-06-23 22:26:43 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
+    int dp[46];
+    int fxn(int n) {
+        if (n <= 2)
+            return n;
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n]=fxn(n - 1) + fxn(n - 2);
+    }
     int climbStairs(int n) {
-        int dp[46];
-        dp[0]=0; dp[1]=1; dp[2]=2;
-	for(int i=3; i<=n; i++){
-	dp[i] = dp[i-1]+dp[i-2];
-	}
-
-	return dp[n];
+        memset(dp, -1, sizeof(dp));
+        return fxn(n);
     }
 };
