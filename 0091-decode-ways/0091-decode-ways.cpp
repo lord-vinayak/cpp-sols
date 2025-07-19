304bb52368e9dd045002898273dce5838b714a48
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2025-07-19 12:31:43 +0530

    0091-DECODE-WAYS Stats: Time: 2 ms (27.47%), Space: 8.5 MB (91.71%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n+1, 0);
+        dp[0] = 1;
+        dp[1] = s[0]!='0'? 1 : 0;
+        for (int i=2; i<=n; i++){
+            if(s[i-1]>='1' && s[i-1]<='9') dp[i] += dp[i-1];
+            if(s[i-2]=='1'||(s[i-2]=='2'&& s[i-1]<='6')) dp[i] += dp[i-2];
+        }
+        return dp[n];
+    }
+};
