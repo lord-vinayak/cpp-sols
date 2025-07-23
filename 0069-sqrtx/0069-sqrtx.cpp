947b0270ef74c96ffb39d7025796c12d0226e077
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2025-07-23 23:45:41 +0530

    0069-SQRTX Stats: Time: 0 ms (100%), Space: 8.4 MB (85.55%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      if (x < 2) {
-            return x;
+        long long y = x;
+        long long lo = 0, hi = y+1;
+        while(lo<hi){
+            long long mid = lo + (hi-lo)/2;
+            if(mid*mid>x) hi = mid;
+            else lo = mid+1;
         }
-        
-        int left = 1, right = x / 2;
-        while (left <= right) {
-            const auto mid = left + (right - left) / 2;
-            if (mid > x / mid) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        return left - 1;  
+        return lo-1;
     }
 };
