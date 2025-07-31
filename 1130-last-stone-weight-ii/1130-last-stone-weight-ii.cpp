2cff1288d7af1aafeb66578b064de045a9a9dadf
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2025-07-31 23:53:09 +0530

    1130-LAST-STONE-WEIGHT-II Stats: Time: 2 ms (57.61%), Space: 12.5 MB (44.38%) - LeetHub

--- 1130-last-stone-weight-ii/1130-last-stone-weight-ii.cpp
+++ 1130-last-stone-weight-ii/1130-last-stone-weight-ii.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        int n = stones.size();
+        int sum = 0;
         int total = 0;
-        for (int stone : stones) {
-            total = total + stone;
+        int n = stones.size();
+        for (int i = 0; i < n; i++) {
+            total += stones[i];
         }
-        vector<int> arr = stones;
-        int sum = total;
-        bool dp[n + 1][sum + 1];
-        for (int i = 0; i < n + 1; ++i) {
-            for (int j = 0; j < sum + 1; ++j) {
-                if (i == 0)
-                    dp[i][j] = false;
-                if (j == 0)
-                    dp[i][j] = true;
-            }
-        }
-        for (int i = 1; i < n + 1; ++i) {
-            for (int j = 1; j < sum + 1; ++j) {
-                if (arr[i - 1] <= j)
-                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
-                else
+        sum = total / 2;
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (stones[i - 1] > j) {
                     dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - stones[i - 1]] +
+                                                     stones[i - 1]);
+                }
             }
         }
-        // vector<int> v;
-        // for(int i=0; i<total/2;i++){
-        // 	if(dp[n][i]==true) v.pb(i);
-        // }
-        // int m = INT_MAX;
-        // for(int i=0; i<v.size(); i++)
-        // 	m = min(m,total-2*v[i]);
-        // cout<<m;
-        int minWeight = INT_MAX;
-
-        for (int i = total / 2; i >= 0; i--) {
-            if (dp[n][i] == true) {
-                minWeight = total - (2 * i);
-                break;
-            }
-        }
-        return minWeight;
+        return total - (2 * dp[n][sum]);
     }
 };
