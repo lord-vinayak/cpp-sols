0368af131fa47ebb830bcef621f33f1f54ec1fb8
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2026-02-19 18:15:29 +0530

    0567-PERMUTATION-IN-STRING Stats: Time: 1 ms (74.61%), Space: 10 MB (45.45%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> mp;
-        for (auto x : s1)
-            mp[x]++;
-        int count = mp.size(), k = s1.size(), i = 0, j = 0;
-        while (j < s2.size()) {
-            if (mp.find(s2[j]) != mp.end()) {
-                mp[s2[j]]--;
-                if (mp[s2[j]] == 0)
-                    count--;
-            }
-            if (j - i + 1 < k)
-                j++;
-            else if (j - i + 1 == k) {
-                if (count == 0)
-                    return true;
-                if (mp.find(s2[i]) != mp.end()) {
-                    mp[s2[i]]++;
-                    if (mp[s2[i]] == 1)
-                        count++;
-                }
-                i++;
-                j++;
-            }
+    bool checkInclusion(string p, string s) {
+        int k = p.size();
+        int n = s.size();
+
+        vector<int> pv(26, 0), sv(26, 0);
+
+        if (n < k)
+            return false;
+
+        for (int i = 0; i < k; i++) {
+            ++pv[p[i] - 'a'];
+            ++sv[s[i] - 'a'];
+        }
+
+        if(pv==sv) return true;
+
+        else{
+            for(int i=k; i<n; i++){
+            ++sv[s[i]-'a']; //add next element
+            --sv[s[i-k]-'a']; //remove leftmost element
+
+            if(pv==sv) return true;
         }
         return false;
+        }
     }
 };
