f9d8a9c99cab84d52db36becc0a8761505008f01
Vinayak S <139204393+lord-vinayak@users.noreply.github.com>
2026-06-21 15:06:01 +0530

    0621-TASK-SCHEDULER Stats: Time: 40 ms (26.9%), Space: 47.8 MB (10.74%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char,int>Map;
-        for (auto ch: tasks)
-            Map[ch]++;
-        
-        int maxFreq = 0;
-        for (auto a:Map)
-            maxFreq = max(maxFreq, a.second);
-        
-        int result = (maxFreq-1)*(n+1);
-        int count = 0;
-        for (auto a:Map)
-        {
-            if (a.second == maxFreq)
-                count++;
+        vector<int> mp(26,0);
+        for(char &ch : tasks) {
+            mp[ch-'A']++;
         }
-        return max(result+count, (int)tasks.size());
+        int time = 0;
+        priority_queue<int> pq;
+        for(int i=0; i<26; i++){
+            if(mp[i]>0) pq.push(mp[i]);
+
+        }
+        while(!pq.empty()){
+            vector<int> temp;
+            for(int i=1; i<=n+1; i++){
+                if(!pq.empty()){
+                    int freq = pq.top();
+                    pq.pop();
+                    freq--;
+                    temp.push_back(freq);
+                }
+            }
+            for(int j:temp){
+                if(j>0) pq.push(j);
+            }
+            if(pq.empty()) time+=temp.size();
+            else time +=n+1;
+        }
+        return time;
     }
 };
